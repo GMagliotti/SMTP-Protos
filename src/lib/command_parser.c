b1ec94f65e51d4ae00e837776c2296bb22d4594e
lucky_number <jfernandezdinardo@itba.edu.ar>
2024-06-15 17:02:27 -0300

    Implement command_parser configuration method

--- src/lib/command_parser.c
+++ src/lib/command_parser.c
@@ -1,6 +1,7 @@
 
 #include "parser.h"
 #include "selector.h"
+#include "command_parser.h"
 
 /** Parser de comandos o "linea" ingresada por el usuario
  *  Estados: S0, S1, S2, S3, SERROR. Siendo S2 y SERROR estados finales
@@ -23,7 +24,8 @@ static void reading_args(struct selector_key * key, u_int8_t c);
 static void error_state_arrival(struct selector_key * key, uint8_t c);
 static void final_state_arrival(struct selector_key * key, u_int8_t c);
 
-static parser_definition command_parser;
+static parser_definition * command_parser;
+static parser_transition ** transitions_list;
 
 enum states {
     S0 = 0,
@@ -61,14 +63,46 @@ static parser_transition S2_transitions[] = {
     {.from_state = S2, .to_state = S3}
 };
 
-void init_command_parsing(){
+void init_command_parsing(smtp_command * smtp_command){
+    //El cliente de este parser de comandos se encarga de asignar la memoria para la estructura del comando
+    if (smtp_command == NULL)
+    {
+        //TODO: loggear error Trying to initialize NULL smtp_command
+        return;
+    }
 
+    smtp_command->parser = &command_parser;
+    //TODO: setear memoria de los vectores de comandos
 }
 
 void parser_configuration(){
+    //Tenemos que asignar memoria para algunos campos del parser_definition
+    command_parser->states = &state_details;
+    command_parser->states_count = STATES_QTY;
+    command_parser->initial_state = &state_details[S0];
+    command_parser->error_state = &state_details[SERROR];
 
+    transitions_list = malloc(STATES_QTY * sizeof(parser_transition *));
+    transitions_list[S0] = S0_transitions;
+    transitions_list[S1] = S1_transitions;
+    transitions_list[S2] = S2_transitions;
+    transitions_list[S3] = NULL;
+    transitions_list[SERROR] = NULL;
+
+    command_parser->transitions = transitions_list;
+    command_parser->transitions_per_state = transitions_per_state;
+
+    //Finalmente configuramos los vectores de booleanos
+    add_rejected_chars_to_transition(&S0_transitions[0], (uint8_t *)" \r\n\0");
+    add_accepted_chars_to_transition(&S0_transitions[1], (uint8_t *)" \0");
+    add_accepted_chars_to_transition(&S0_transitions[2], (uint8_t *)"\r\0");
+
+    add_rejected_chars_to_transition(&S1_transitions[0], (uint8_t *)"\r\n\0");
+    add_accepted_chars_to_transition(&S1_transitions[1], (uint8_t *)"\r");
+
+    add_accepted_chars_to_transition(&S2_transitions[0], (uint8_t *)"\n");
 }
 
 void finish_command_parsing(){
-
+    free(transitions_list);
 }
--- src/lib/headers/command_parser.h
+++ src/lib/headers/command_parser.h
@@ -2,6 +2,9 @@
 #define COMMAND_PARSER_H
 
 #include <parser.h>
+#include <selector.h>
+
+#define STATES_QTY 5
 
 /** Estructura que ira recolectando caracteres hasta armar un comando smtp capaz de ser procesado.
  *  Un puntero al command_parser debe ser necesario para ir procesando cada caracter**/
@@ -13,11 +16,13 @@ typedef struct smtp_command
 } smtp_command;
 
 /** Inicializamos el parseo de comandos **/
-void init_command_parsing();
+void init_command_parsing(smtp_command * smtp_command);
 
 /** Configuracion del parser de comandos, armamado de la estructura parser_definition, setup de vectores de chars aceptados**/
 void parser_configuration();
 
+/** Encargada de ir parseando el comando, es la funcion que llamara a process_char. Retorna el estado que corresponde **/
+int parse_command(struct selector_key * key);
 
 void finish_command_parsing();
 
