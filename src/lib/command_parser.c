e89cbb994993e4065a1ed4c1f2c1f4322c865633
Francisco F <fferrutti@itba.edu.ar>
2024-06-16 22:47:18 -0300

    minor fix: error handling in parser.c in function parser_feed()

--- src/lib/command_parser.c
+++ src/lib/command_parser.c
@@ -1,174 +1,173 @@
 
+#include "command_parser.h"
+
 #include "parser.h"
 #include "selector.h"
-#include "command_parser.h"
 #include "smtp.h"
 
 /** Parser de comandos o "linea" ingresada por el usuario
  *  Estados: S0, S1, S2, S3, SERROR. Siendo S2 y SERROR estados finales
  *  Transiciones:
- *  
+ *
  *  S0 -> ~{' ', \r, \n} -> S0
  *  S0 -> {' '} -> S1
  *  S0 -> {\r} -> S2
- * 
+ *
  *  S1 -> ~{\r, \n} -> S1
  *  S1 -> {\r} -> S2
- * 
+ *
  *  S2 -> {\n} -> S3
- * 
+ *
  * **/
 
-
-static void reading_command(struct selector_key * key, u_int8_t c);
-static void reading_args(struct selector_key * key, u_int8_t c);
-static void error_state_arrival(struct selector_key * key, uint8_t c);
-static void final_state_arrival(struct selector_key * key, u_int8_t c);
+static void reading_command(struct selector_key* key, u_int8_t c);
+static void reading_args(struct selector_key* key, u_int8_t c);
+static void error_state_arrival(struct selector_key* key, uint8_t c);
+static void final_state_arrival(struct selector_key* key, u_int8_t c);
 
 static parser_definition command_parser;
-static parser_transition *transitions_list[STATES_QTY];
+static parser_transition* transitions_list[STATES_QTY];
 
-enum states {
-    S0 = 0,
-    S1,
-    S2,
-    S3,
-    SERROR
-};
-
-static parser_state state_details[] = {
-    {.id = S0, .is_final = false, .on_departure = reading_command},
-    {.id = S1, .is_final = false, .on_departure = reading_args},
-    {.id = S2, .is_final = false},
-    {.id = S3, .is_final = true, .on_arrival = final_state_arrival},
-    {.id = SERROR, .is_final = true, .on_arrival = error_state_arrival}
-} ;
+static parser_state state_details[] = { { .id = S0, .is_final = false, .on_departure = reading_command },
+	                                    { .id = S1, .is_final = false, .on_departure = reading_args },
+	                                    { .id = S2, .is_final = false },
+	                                    { .id = S3, .is_final = true, .on_arrival = final_state_arrival },
+	                                    { .id = SERROR, .is_final = true, .on_arrival = error_state_arrival } };
 
 static size_t transitions_per_state[] = {
-    3, 2, 1, 0, 0
-    // S0, S1, ...
+	3,
+	2,
+	1,
+	0,
+	0
+	// S0, S1, ...
 };
 
-static parser_transition S0_transitions[] = {
-    {.from_state = S0, .to_state = S0},
-    {.from_state = S0, .to_state = S1},
-    {.from_state = S0, .to_state = S2}
-};
+static parser_transition S0_transitions[] = { { .from_state = S0, .to_state = S0 },
+	                                          { .from_state = S0, .to_state = S1 },
+	                                          { .from_state = S0, .to_state = S2 } };
 
-static parser_transition S1_transitions[] = {
-    {.from_state = S1, .to_state = S1},
-    {.from_state = S1, .to_state = S2}
-};
+static parser_transition S1_transitions[] = { { .from_state = S1, .to_state = S1 },
+	                                          { .from_state = S1, .to_state = S2 } };
 
-static parser_transition S2_transitions[] = {
-    {.from_state = S2, .to_state = S3}
-};
+static parser_transition S2_transitions[] = { { .from_state = S2, .to_state = S3 } };
 
-void init_command_parsing(smtp_command * smtp_command){
-    //El cliente de este parser de comandos se encarga de asignar la memoria para la estructura del comando
-    if (smtp_command == NULL)
-    {
-        //TODO: loggear error Trying to initialize NULL smtp_command
-        return;
-    }
+void
+init_command_parsing(smtp_command* smtp_command)
+{
+	// El cliente de este parser de comandos se encarga de asignar la memoria para la estructura del comando
+	if (smtp_command == NULL) {
+		// TODO: loggear error Trying to initialize NULL smtp_command
+		return;
+	}
 
-    smtp_command->parser = &command_parser;
-    smtp_command->ended = false;
-    smtp_command->error = false;
-    smtp_command->current_state = S0;
-    smtp_command->command_dim = 0;
-    smtp_command->arg_dim = 0;
+	smtp_command->parser = &command_parser;
+	smtp_command->ended = false;
+	smtp_command->error = false;
+	smtp_command->current_state = S0;
+	smtp_command->command_dim = 0;
+	smtp_command->arg_dim = 0;
 
-    memset(smtp_command->command, 0, MAX_COMMAND_LEN + 1);
-    memset(smtp_command->arg, 0, MAX_LINE_LEN - MAX_COMMAND_LEN - 3 + 1);
+	memset(smtp_command->command, 0, MAX_COMMAND_LEN + 1);
+	memset(smtp_command->arg, 0, MAX_LINE_LEN - MAX_COMMAND_LEN - 3 + 1);
 }
 
-void parser_configuration(){
-    //Tenemos que asignar memoria para algunos campos del parser_definition
-    command_parser.states = state_details;
-    command_parser.states_count = STATES_QTY;
-    command_parser.initial_state = &state_details[S0];
-    command_parser.error_state = &state_details[SERROR];
+void
+parser_configuration()
+{
+	// Tenemos que asignar memoria para algunos campos del parser_definition
+	command_parser.states = state_details;
+	command_parser.states_count = STATES_QTY;
+	command_parser.initial_state = &state_details[S0];
+	command_parser.error_state = &state_details[SERROR];
 
-    transitions_list[S0] = S0_transitions;
-    transitions_list[S1] = S1_transitions;
-    transitions_list[S2] = S2_transitions;
-    transitions_list[S3] = NULL;
-    transitions_list[SERROR] = NULL;
+	transitions_list[S0] = S0_transitions;
+	transitions_list[S1] = S1_transitions;
+	transitions_list[S2] = S2_transitions;
+	transitions_list[S3] = NULL;
+	transitions_list[SERROR] = NULL;
 
-    command_parser.transitions = transitions_list;
-    command_parser.transitions_per_state = transitions_per_state;
+	command_parser.transitions = transitions_list;
+	command_parser.transitions_per_state = transitions_per_state;
 
-    //Finalmente configuramos los vectores de booleanos
-    add_rejected_chars_to_transition(&S0_transitions[0], (uint8_t *)" \r\n\0");
-    add_accepted_chars_to_transition(&S0_transitions[1], (uint8_t *)" \0");
-    add_accepted_chars_to_transition(&S0_transitions[2], (uint8_t *)"\r\0");
+	// Finalmente configuramos los vectores de booleanos
+	add_rejected_chars_to_transition(&S0_transitions[0], (uint8_t*)" \r\n\0");
+	add_accepted_chars_to_transition(&S0_transitions[1], (uint8_t*)" \0");
+	add_accepted_chars_to_transition(&S0_transitions[2], (uint8_t*)"\r\0");
 
-    add_rejected_chars_to_transition(&S1_transitions[0], (uint8_t *)"\r\n\0");
-    add_accepted_chars_to_transition(&S1_transitions[1], (uint8_t *)"\r");
+	add_rejected_chars_to_transition(&S1_transitions[0], (uint8_t*)"\r\n\0");
+	add_accepted_chars_to_transition(&S1_transitions[1], (uint8_t*)"\r");
 
-    add_accepted_chars_to_transition(&S2_transitions[0], (uint8_t *)"\n");
+	add_accepted_chars_to_transition(&S2_transitions[0], (uint8_t*)"\n");
 }
 
-int parse_command(struct selector_key * key, smtp_command * smtp_command, struct buffer * buffer){
-    //smtp_data * client_data = ATTACHMENT(key);
-    int state = 0;
-    //Tenemos que empezar a recorrer la maquina de estados
-    //Cuando finalizamos? Cuando caemos en un estado de error o final o cuando no hay mas para leer
-    //Para recorrer la maquina de estados usamos la referencia al parser dentro del smtp_command
-    while (smtp_command->current_state != SERROR && buffer_can_read(buffer) && smtp_command->ended != true)
-    {
-        state = parser_feed(key, smtp_command->parser, smtp_command->current_state, buffer_read(buffer));
-        smtp_command->current_state = state;
-    }
-    
-    return state;
-}
+int
+parse_command(struct selector_key* key, smtp_command* smtp_command, struct buffer* buffer)
+{
+	// smtp_data * client_data = ATTACHMENT(key);
+	int state = 0;
+	// Tenemos que empezar a recorrer la maquina de estados
+	// Cuando finalizamos? Cuando caemos en un estado de error o final o cuando no hay mas para leer
+	// Para recorrer la maquina de estados usamos la referencia al parser dentro del smtp_command
+	while (smtp_command->current_state != SERROR && buffer_can_read(buffer) && smtp_command->ended != true) {
+		state = parser_feed(key, smtp_command->parser, smtp_command->current_state, buffer_read(buffer));
+		smtp_command->current_state = state;
+	}
 
+	return state;
+}
 
 /** Estamos en S0 y entra un char, empezamos a procesar. SMTP NO ES case sensitive **/
-static void reading_command(struct selector_key * key, u_int8_t c){
-    smtp_data * client_data = ATTACHMENT(key);
-    smtp_command * smtp_command = &client_data->command_parser;
+static void
+reading_command(struct selector_key* key, u_int8_t c)
+{
+	smtp_data* client_data = ATTACHMENT(key);
+	smtp_command* smtp_command = &client_data->command_parser;
 
-    if (smtp_command->command_dim >= MAX_COMMAND_LEN){
-        //No es comando valido
-        //TODO: Aca hay que llamar al error handler o algo que indique que el comando es bochado
-        //o capaz no se maneja aca, ni idea ajaj
-        return;
-    }
-    smtp_command->command[smtp_command->command_dim] = c;
-    smtp_command->command_dim++;
-    
-    return;
+	if (smtp_command->command_dim >= MAX_COMMAND_LEN) {
+		// No es comando valido
+		// TODO: Aca hay que llamar al error handler o algo que indique que el comando es bochado
+		// o capaz no se maneja aca, ni idea ajaj
+		return;
+	}
+	smtp_command->command[smtp_command->command_dim] = c;
+	smtp_command->command_dim++;
+
+	return;
 }
 
 /** Estamos en S1 y entra un char, empezamos a procesar.**/
-static void reading_args(struct selector_key * key, u_int8_t c){
-    smtp_data * client_data = ATTACHMENT(key);
-    smtp_command * smtp_command = &client_data->command_parser;
+static void
+reading_args(struct selector_key* key, u_int8_t c)
+{
+	smtp_data* client_data = ATTACHMENT(key);
+	smtp_command* smtp_command = &client_data->command_parser;
 
-    if (smtp_command->arg_dim >= MAX_ARG_LEN) {
-        //No es argumento valido
-        //TODO: Aca hay que llamar al error handler o algo que indique que el comando es bochado
-        //o capaz no se maneja aca, ni idea ajaj
-        return;
-    }
-    smtp_command->arg[smtp_command->arg_dim] = c;
-    smtp_command->arg_dim++;
+	if (smtp_command->arg_dim >= MAX_ARG_LEN) {
+		// No es argumento valido
+		// TODO: Aca hay que llamar al error handler o algo que indique que el comando es bochado
+		// o capaz no se maneja aca, ni idea ajaj
+		return;
+	}
+	smtp_command->arg[smtp_command->arg_dim] = c;
+	smtp_command->arg_dim++;
 
-    return;
+	return;
 }
 
-static void final_state_arrival(struct selector_key * key, u_int8_t c){
-    smtp_data * client_data = ATTACHMENT(key);
-    smtp_command * smtp_command = &client_data->command_parser;
-    smtp_command->ended = true;
+static void
+final_state_arrival(struct selector_key* key, u_int8_t c)
+{
+	smtp_data* client_data = ATTACHMENT(key);
+	smtp_command* smtp_command = &client_data->command_parser;
+	smtp_command->ended = true;
 }
 
-static void error_state_arrival(struct selector_key * key, uint8_t c){
-    smtp_data * client_data = ATTACHMENT(key);
-    smtp_command * smtp_command = &client_data->command_parser;
-    smtp_command->ended = true;
-    smtp_command->error = true;
+static void
+error_state_arrival(struct selector_key* key, uint8_t c)
+{
+	smtp_data* client_data = ATTACHMENT(key);
+	smtp_command* smtp_command = &client_data->command_parser;
+	smtp_command->ended = true;
+	smtp_command->error = true;
 }
--- src/lib/parser.c
+++ src/lib/parser.c
@@ -1,27 +1,31 @@
 #include "parser.h"
 
+#include "command_parser.h"
+
 #include <assert.h>
 #include <stdbool.h>
 #include <stdlib.h>
 #include <string.h>
 
-int parser_feed(struct selector_key * key, parser_definition * parser, unsigned current_state_id, uint8_t c){
-	if (parser == NULL){
-		//TODO: loggear error NULL parser
-		return -1;
+int
+parser_feed(struct selector_key* key, parser_definition* parser, unsigned current_state_id, uint8_t c)
+{
+	if (parser == NULL) {
+		// TODO: loggear error NULL parser
+		return SERROR;
 	}
 
 	size_t char_index;
 
-	//Obtenemos la posicion del char en el array de booleanos (caracteres permitidos por la transicion)
-	if(c < 32 || c > 127) {
+	// Obtenemos la posicion del char en el array de booleanos (caracteres permitidos por la transicion)
+	if (c < 32 || c > 127) {
 		if (c == '\r') {
 			char_index = CR_INDEX;
 		} else if (c == '\n') {
 			char_index = LF_INDEX;
 		} else {
-			//TODO: loggear error invalid character
-			return -1;
+			// TODO: loggear error invalid character
+			return SERROR;
 		}
 	} else {
 		if (c == ' ') {
@@ -31,57 +35,55 @@ int parser_feed(struct selector_key * key, parser_definition * parser, unsigned 
 		}
 	}
 
-	//Buscamos si hay alguna transicion que acepte el caracter, para eso primero obtenemos todas las transiciones del estado actual
-	parser_transition * transitions = parser->transitions[current_state_id];	
+	// Buscamos si hay alguna transicion que acepte el caracter, para eso primero obtenemos todas las transiciones del
+	// estado actual
+	parser_transition* transitions = parser->transitions[current_state_id];
 	size_t transitions_amount = parser->transitions_per_state[current_state_id];
-	for (size_t i = 0; i < transitions_amount; i++)
-	{
-		//Char aceptado
-		if(transitions[i].accepted_chars[char_index]){
-			//Vemos si hay un metodo de salida de estado
-			parser_state * from_state = &parser->states[transitions[i].from_state];
-			if(from_state->on_departure != NULL) {
+	for (size_t i = 0; i < transitions_amount; i++) {
+		// Char aceptado
+		if (transitions[i].accepted_chars[char_index]) {
+			// Vemos si hay un metodo de salida de estado
+			parser_state* from_state = &parser->states[transitions[i].from_state];
+			if (from_state->on_departure != NULL) {
 				from_state->on_departure(key, c);
 			}
-			//Vemos si hay un metodo de entrada para el nuevo estado
-			parser_state * to_state = &parser->states[transitions[i].to_state];
-			if(to_state->on_arrival != NULL) {
+			// Vemos si hay un metodo de entrada para el nuevo estado
+			parser_state* to_state = &parser->states[transitions[i].to_state];
+			if (to_state->on_arrival != NULL) {
 				to_state->on_arrival(key, c);
 			}
 
 			return to_state->id;
 		}
-
 	}
-	
-	//No se encontro ninguna transicion para el char, por lo tanto no fue aceptado
-	//Por lo tanto, una opcion ahora es ir al estado de error
-	if(parser->states[current_state_id].on_departure != NULL){
+
+	// No se encontro ninguna transicion para el char, por lo tanto no fue aceptado
+	// Por lo tanto, una opcion ahora es ir al estado de error
+	if (parser->states[current_state_id].on_departure != NULL) {
 		parser->states[current_state_id].on_departure(key, c);
 	}
 
-	if(parser->error_state->on_arrival != NULL){
+	if (parser->error_state->on_arrival != NULL) {
 		parser->error_state->on_arrival(key, c);
 	}
 
 	return parser->error_state->id;
 }
 
-void add_accepted_chars_to_transition(parser_transition * transition, uint8_t * array_chars){
-	
-	//Primero seteamos todas las posiciones en falso
-	//TODO: no esta buena esta implementacion porque no puedo ir agregando chars de a varias llamadas.
-	
-	for (size_t i = 0; i < PRINTABLE_ASCII_CHARS; i++)
-	{
+void
+add_accepted_chars_to_transition(parser_transition* transition, uint8_t* array_chars)
+{
+	// Primero seteamos todas las posiciones en falso
+	// TODO: no esta buena esta implementacion porque no puedo ir agregando chars de a varias llamadas.
+
+	for (size_t i = 0; i < PRINTABLE_ASCII_CHARS; i++) {
 		transition->accepted_chars[i] = false;
 	}
-	
+
 	uint8_t c;
-	for (size_t i = 0; array_chars[i] != '\0'; i++)
-	{
+	for (size_t i = 0; array_chars[i] != '\0'; i++) {
 		c = array_chars[i];
-		if(c < 32 || c > 127) {
+		if (c < 32 || c > 127) {
 			if (c == '\r') {
 				transition->accepted_chars[CR_INDEX] = true;
 			} else if (c == '\n') {
@@ -99,21 +101,20 @@ void add_accepted_chars_to_transition(parser_transition * transition, uint8_t * 
 	}
 }
 
-void add_rejected_chars_to_transition(parser_transition * transition, u_int8_t * array_chars){
-	
-	//Primero seteamos todas las posiciones en verdadero
-	//TODO: no esta buena esta implementacion porque no puedo ir agregando chars de a varias llamadas.
-	
-	for (size_t i = 0; i < PRINTABLE_ASCII_CHARS; i++)
-	{
+void
+add_rejected_chars_to_transition(parser_transition* transition, u_int8_t* array_chars)
+{
+	// Primero seteamos todas las posiciones en verdadero
+	// TODO: no esta buena esta implementacion porque no puedo ir agregando chars de a varias llamadas.
+
+	for (size_t i = 0; i < PRINTABLE_ASCII_CHARS; i++) {
 		transition->accepted_chars[i] = true;
 	}
 
 	uint8_t c;
-	for (size_t i = 0; array_chars[i] != '\0'; i++)
-	{
+	for (size_t i = 0; array_chars[i] != '\0'; i++) {
 		c = array_chars[i];
-		if(c < 32 || c > 127) {
+		if (c < 32 || c > 127) {
 			if (c == '\r') {
 				transition->accepted_chars[CR_INDEX] = false;
 			} else if (c == '\n') {
@@ -131,6 +132,8 @@ void add_rejected_chars_to_transition(parser_transition * transition, u_int8_t *
 	}
 }
 
-bool is_final(parser_state * state){
+bool
+is_final(parser_state* state)
+{
 	return state->is_final;
 }
