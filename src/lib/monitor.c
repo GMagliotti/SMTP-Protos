7e40074cde88ad9f9832563368dfb9a72efb15d9
Francisco F <fferrutti@itba.edu.ar>
2024-06-21 21:43:47 -0300

    monitor integrated

---
+++ src/lib/monitor.c
@@ -0,0 +1,239 @@
+#include "monitor.h"
+
+#include "selector.h"
+
+#include <arpa/inet.h>
+#include <stdlib.h>
+#include <sys/socket.h>
+#include <unistd.h>  // this library is for the Network Order functions
+
+// the monitor protocol is a simple protocol that allows the client to send a message to the server
+#define SIGNATURE 0xfffe
+#define VERSION   0x00
+#define TOKEN     0xffe91a2b3c4d5e6f
+
+enum commands
+{
+	CMD_HIST_C = 0x00,
+	CMD_CONC_C,
+	CMD_BYTES_T,
+	CMD_TRANS_S,
+	CMD_TRANS_ON,
+	CMD_TRANS_OFF
+};
+
+enum status
+{
+	S_SUCCESS = 0x00,
+	S_INV_VERS,
+	S_AUTH_FAIL,
+	S_INV_CMD,
+	S_INV_REQ_LEN,
+	S_UNEXPECTED,
+	S_SIGNATURE_ERR
+};
+
+void
+monitor_done(selector_key* key)
+{
+	monitor_data* data = ATTACHMENT(key);
+	free(data);
+}
+
+int
+is_valid_command(uint8_t command)
+{
+	switch (command) {
+		case 0x00:
+		case 0x01:
+		case 0x02:
+		case 0x03:
+		case 0x04:
+		case 0x05:
+			return 1;
+		default:
+			return -1;
+	}
+}
+
+ssize_t
+send_response(int sockfd, const void* buf, size_t len, int flags, const struct sockaddr* dest_addr, socklen_t addrlen)
+{
+	ssize_t sent_bytes = sendto(sockfd, buf, len, flags, dest_addr, addrlen);
+	if (sent_bytes < 0) {
+		perror("sendto");
+		// Handle error
+	}
+	return sent_bytes;
+}
+
+void
+process_valid_command(uint8_t command, uint8_t* response, uint8_t* status)
+{
+	*status = S_SUCCESS;
+	uint16_t val;
+	switch (command) {
+		case 0x00:
+			val = 10503;
+			val = htons(val);
+			response[6] = (val >> 8) & 0xff;
+			response[7] = val & 0xff;
+			break;
+		case 0x01:
+		case 0x02:
+			response[6] = 0x29;  // hardcoded
+			response[7] = 0x07;
+			response[8] = 0x00;
+			response[9] = 0x00;
+			response[10] = 0x00;
+			response[11] = 0x00;
+			response[12] = 0x00;
+			response[13] = 0x00;
+			break;
+		case 0x03:
+			response[6] = 0x03;
+			break;
+		case 0x04:
+			response[6] = 0x04;
+			break;
+		case 0x05:
+			response[6] = 0x05;
+			break;
+		default:
+			break;
+	}
+}
+
+int
+parse_monitor_message(const uint8_t* buffer,
+                      size_t n,
+                      uint8_t* data_command,
+                      uint16_t* data_request_id,
+                      uint8_t* response,
+                      uint8_t* status)
+{
+	// we check if the message is a monitor message.
+	// a monitor message is a message that has the following format:
+	// 2 bytes for the signature
+	// 1 byte for the version
+	// 2 bytes for the request id
+	// 8 bytes for the token
+	// 1 byte for the command
+
+	if (n < 14) {
+		return -1;
+	}
+
+	uint16_t signature = buffer[0] << 8 | buffer[1];
+	signature = ntohs(signature);
+
+	uint8_t version = buffer[2];
+
+	uint16_t request_id = buffer[3] << 8 | buffer[4];
+	uint16_t request_id_from_net = ntohs(request_id);
+
+	uint8_t token_arr[8];
+	for (int i = 0; i < 8; i++) {
+		token_arr[i] = buffer[5 + i];
+	}
+
+	uint64_t token = 0;
+	for (int i = 0; i < 8; i++) {
+		token = token << 8 | token_arr[i];
+	}
+
+	uint8_t command = buffer[13];
+
+	if (signature != SIGNATURE) {
+		return -1;
+	}
+	if (version != VERSION) {
+		*status = S_INV_VERS;
+		return -1;
+	}
+	if (token != TOKEN) {
+		*status = S_AUTH_FAIL;
+		return -1;
+	}
+	if (is_valid_command(command) < 0) {
+		*status = S_INV_CMD;
+		return -1;
+	}
+	if (n != 14) {
+		*status = S_INV_REQ_LEN;
+		return -1;
+	}
+	*status = S_SUCCESS;
+
+	// we have a valid message
+
+	// we write into the response buffer (we are not managing the command here)
+
+	// 2 bytes for the signature
+	signature = htons((uint16_t)SIGNATURE);
+	response[0] = (signature >> 8) & 0xff;
+	response[1] = signature & 0xff;
+
+	// 1 byte for the version
+	response[2] = VERSION;
+
+	// 2 bytes for the request id
+	response[3] = buffer[3];
+	response[4] = buffer[4];
+
+	// 1 byte for the status
+	response[5] = S_SUCCESS;
+
+	*data_command = command;
+	*data_request_id = request_id_from_net;
+
+	return 1;
+}
+
+void
+handle_udp_packet(struct selector_key* key)
+{
+	monitor_data* data = calloc(1, sizeof(*data));
+	if (data == NULL) {
+		return;
+	}
+
+	data->client_addr_len = sizeof(data->client_addr);
+
+	int bytes_read = recvfrom(
+	    key->fd, data->raw_buff_read, BUFFER_SIZE, 0, (struct sockaddr*)&data->client_addr, &data->client_addr_len);
+
+	if (bytes_read < 0) {
+		perror("recvfrom");
+		monitor_done(key);
+		return;
+	}
+
+	uint8_t status = S_SIGNATURE_ERR;
+
+	if (parse_monitor_message(
+	        data->raw_buff_read, bytes_read, &data->command, &data->request_id, data->raw_buff_write, &status) > 0) {
+		// valid command, we send the response
+
+		// should we do this in a separate thread?
+		process_valid_command(data->command, data->raw_buff_write, &status);
+
+		send_response(
+		    key->fd, data->raw_buff_write, BUFFER_SIZE, 0, (struct sockaddr*)&data->client_addr, data->client_addr_len);
+
+	} else {
+		if (status < S_SIGNATURE_ERR) {  // if status = S_SIGNATURE_ERR, we don't send a response
+			// we send the error response
+			data->raw_buff_write[5] = status;
+			send_response(key->fd,
+			              data->raw_buff_write,
+			              BUFFER_SIZE,
+			              0,
+			              (struct sockaddr*)&data->client_addr,
+			              data->client_addr_len);
+		}
+	}
+
+	// we free the data
+	monitor_done(key);
+}
--- src/main.c
+++ src/main.c
@@ -10,6 +10,7 @@
  * DNS utilizando getaddrinfo), pero toda esa complejidad está oculta en
  * el selector.
  */
+#include "lib/headers/monitor.h"
 #include "lib/headers/selector.h"
 #include "lib/headers/smtp.h"
 
@@ -37,6 +38,7 @@ int
 main(const int argc, const char** argv)
 {
 	unsigned port = 2525;
+	unsigned monitor_port = 2526;
 
 	if (argc == 1) {
 		// utilizamos el default
@@ -47,6 +49,10 @@ main(const int argc, const char** argv)
 		if (end == argv[1] || '\0' != *end || ((LONG_MIN == sl || LONG_MAX == sl) && ERANGE == errno) || sl < 0 ||
 		    sl > USHRT_MAX) {
 			fprintf(stderr, "port should be an integer: %s\n", argv[1]);
+			return 1;
+		}
+		if (sl == 2526) {
+			fprintf(stderr, "port %d is reserved for monitoring\n", sl);
 			return 1;
 		}
 		port = sl;
@@ -66,6 +72,11 @@ main(const int argc, const char** argv)
 	struct sockaddr_in addr4;
 	memset(&addr6, 0, sizeof(addr6));
 	memset(&addr4, 0, sizeof(addr4));
+	// sockaddrs for monitoring
+	struct sockaddr_in6 monitor_addr6;
+	struct sockaddr_in monitor_addr4;
+	memset(&monitor_addr6, 0, sizeof(monitor_addr6));
+	memset(&monitor_addr4, 0, sizeof(monitor_addr4));
 
 	addr6.sin6_family = AF_INET6;   // IPv6
 	addr6.sin6_addr = in6addr_any;  // any local address, filter
@@ -74,6 +85,15 @@ main(const int argc, const char** argv)
 	addr4.sin_family = AF_INET;                 // IPv4
 	addr4.sin_addr.s_addr = htonl(INADDR_ANY);  // any local address, filter
 	addr4.sin_port = htons(port);               // set port
+
+	// for monitoring
+	monitor_addr6.sin6_family = AF_INET6;           // IPv6
+	monitor_addr6.sin6_addr = in6addr_any;          // any local address, filter
+	monitor_addr6.sin6_port = htons(monitor_port);  // set port
+
+	monitor_addr4.sin_family = AF_INET;                 // IPv4
+	monitor_addr4.sin_addr.s_addr = htonl(INADDR_ANY);  // any local address, filter
+	monitor_addr4.sin_port = htons(monitor_port);       // set port
 
 	// create sockets
 	const int server6 = socket(AF_INET6, SOCK_STREAM, IPPROTO_TCP);
@@ -88,15 +108,72 @@ main(const int argc, const char** argv)
 		goto finally;
 	}
 
+	// for monitoring
+
+	const int monitor_server6 = socket(AF_INET6, SOCK_STREAM, IPPROTO_TCP);
+	if (monitor_server6 < 0) {
+		err_msg = "unable to create IPv6 monitoring socket";
+		goto finally;
+	}
+
+	const int monitor_server4 = socket(AF_INET, SOCK_STREAM, 0);
+	if (monitor_server4 < 0) {
+		err_msg = "unable to create IPv4 monitoring socket";
+		goto finally;
+	}
+
 	fprintf(stdout, "Listening on TCP port %d\n", port);
+	fprintf(stdout, "Listening on UDP port %d for monitoring\n", monitor_port);
 
 	// man 7 ip. no importa reportar nada si falla.
 
 	setsockopt(server6, SOL_SOCKET, SO_REUSEADDR, &(int){ 1 }, sizeof(int));
 	setsockopt(server4, SOL_SOCKET, SO_REUSEADDR, &(int){ 1 }, sizeof(int));
+	setsockopt(monitor_server6, SOL_SOCKET, SO_REUSEADDR, &(int){ 1 }, sizeof(int));
+	setsockopt(monitor_server4, SOL_SOCKET, SO_REUSEADDR, &(int){ 1 }, sizeof(int));
 
 	// nos aseguramos que el socket IPv6 no escuche en direcciones IPv4
 	setsockopt(server6, IPPROTO_IPV6, IPV6_V6ONLY, &(int){ 1 }, sizeof(int));
+	setsockopt(monitor_server6, IPPROTO_IPV6, IPV6_V6ONLY, &(int){ 1 }, sizeof(int));
+
+	// UDP close operation is blocking, so we use SO_LINGER to make it non-blocking
+	int option_len = sizeof(struct linger);
+	struct linger linger;
+
+	int rc = getsockopt(monitor_server4, SOL_SOCKET, SO_LINGER, (void*)&linger, (socklen_t* restrict)&option_len);
+
+	if (rc < 0) {
+		perror("getsockopt");
+		return 1;
+	}
+
+	linger.l_onoff = 1;
+	linger.l_linger = 0;
+
+	rc = setsockopt(monitor_server4, SOL_SOCKET, SO_LINGER, (void*)&linger, option_len);
+
+	if (rc < 0) {
+		perror("setsockopt");
+		return 1;
+	}
+
+	// same for IPv6
+	rc = getsockopt(monitor_server6, SOL_SOCKET, SO_LINGER, (void*)&linger, (socklen_t* restrict)&option_len);
+
+	if (rc < 0) {
+		perror("getsockopt");
+		return 1;
+	}
+
+	linger.l_onoff = 1;
+	linger.l_linger = 0;
+
+	// now set the option back
+	rc = setsockopt(monitor_server6, SOL_SOCKET, SO_LINGER, (void*)&linger, option_len);
+	if (rc < 0) {
+		perror("setsockopt");
+		return 1;
+	}
 
 	// bind sockets to IP and port
 	if (bind(server6, (struct sockaddr*)&addr6, sizeof(addr6)) < 0) {
@@ -106,6 +183,16 @@ main(const int argc, const char** argv)
 
 	if (bind(server4, (struct sockaddr*)&addr4, sizeof(addr4)) < 0) {
 		err_msg = "unable to bind IPv4 socket";
+		goto finally;
+	}
+
+	if (bind(monitor_server6, (struct sockaddr*)&monitor_addr6, sizeof(monitor_addr6)) < 0) {
+		err_msg = "unable to bind IPv6 monitoring socket";
+		goto finally;
+	}
+
+	if (bind(monitor_server4, (struct sockaddr*)&monitor_addr4, sizeof(monitor_addr4)) < 0) {
+		err_msg = "unable to bind IPv4 monitoring socket";
 		goto finally;
 	}
 
@@ -132,6 +219,15 @@ main(const int argc, const char** argv)
 	}
 	if (selector_fd_set_nio(server4) == -1) {
 		err_msg = "getting server IPv4 socket flags";
+		goto finally;
+	}
+
+	if (selector_fd_set_nio(monitor_server6) == -1) {
+		err_msg = "getting server IPv6 monitoring socket flags";
+		goto finally;
+	}
+	if (selector_fd_set_nio(monitor_server4) == -1) {
+		err_msg = "getting server IPv4 monitoring socket flags";
 		goto finally;
 	}
 
@@ -163,6 +259,12 @@ main(const int argc, const char** argv)
 		.handle_close = NULL,  // nada que liberar
 	};
 
+	const struct fd_handler monitor = {
+		.handle_read = handle_udp_packet,
+		.handle_write = NULL,
+		.handle_close = NULL,  // nada que liberar
+	};
+
 	// register servers fd to selector in readfds set
 	ss = selector_register(selector, server6, &smtp, OP_READ, NULL);
 	if (ss != SELECTOR_SUCCESS) {
@@ -173,6 +275,18 @@ main(const int argc, const char** argv)
 	ss = selector_register(selector, server4, &smtp, OP_READ, NULL);
 	if (ss != SELECTOR_SUCCESS) {
 		err_msg = "registering IPv4 fd";
+		goto finally;
+	}
+
+	ss = selector_register(selector, monitor_server6, &monitor, OP_READ, NULL);
+	if (ss != SELECTOR_SUCCESS) {
+		err_msg = "registering IPv6 monitoring fd";
+		goto finally;
+	}
+
+	ss = selector_register(selector, monitor_server4, &monitor, OP_READ, NULL);
+	if (ss != SELECTOR_SUCCESS) {
+		err_msg = "registering IPv4 monitoring fd";
 		goto finally;
 	}
 
