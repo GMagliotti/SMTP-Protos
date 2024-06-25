e96e0e981f74fbec20cf416b70680e2737ad1c90
Gianfranco Magliotti <gmagliotti@itba.edu.ar>
2024-06-25 01:48:15 -0300

    Add test for concurrent connections

--- src/test/concurrency_test.c
+++ src/test/concurrency_test.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <stdbool.h>
 #include <unistd.h>
 #include <pthread.h>
 #include <sys/socket.h>
@@ -9,7 +10,7 @@
 
 #define SMTPD_PORT 2525
 #define SMTPD_SERVER "127.0.0.1"
-#define NUM_THREADS 100
+#define NUM_THREADS 500
 
 void *send_email(void *threadid) {
     long tid;
@@ -43,6 +44,7 @@ void *send_email(void *threadid) {
     servaddr.sin_family = AF_INET;
     servaddr.sin_port = htons(SMTPD_PORT);
     servaddr.sin_addr.s_addr = inet_addr(SMTPD_SERVER);
+    char buffer[2];
 
     // Connect to the server
     if (connect(sockfd, (struct sockaddr *)&servaddr, sizeof(servaddr)) < 0) {
@@ -50,20 +52,36 @@ void *send_email(void *threadid) {
         close(sockfd);
         pthread_exit(NULL);
     }
+    sleep(1);
 
     // Send HELO command
     char *helo = "HELO example.com\r\n";
     write(sockfd, helo, strlen(helo));
+    bool fcont = true;
+    while ( fcont ) {
+        read(sockfd, buffer, 1);
+        fcont = buffer[0] == '\n'?false:true;
+    }
 
-    sleep(1);
+
     // MAIL FROM
     char *mail_from = "MAIL FROM:<sender@example.com>\r\n";
     write(sockfd, mail_from, strlen(mail_from));
+    fcont = true;
+    while ( fcont ) {
+        read(sockfd, buffer, 1);
+        fcont = buffer[0] == '\n'?false:true;
+    }
 
-    sleep(1);
     // RCPT TO
     char *rcpt_to_one = "RCPT TO:<recipient1@example.com>\r\n";
     write(sockfd, rcpt_to_one, strlen(rcpt_to_one));
+    fcont = true;
+    while ( fcont ) {
+        read(sockfd, buffer, 1);
+        fcont = buffer[0] == '\n'?false:true;
+    }
+
 
     // char *rcpt_to_two = "RCPT TO:<recipient2@example.com>\r\n";
     // write(sockfd, rcpt_to_two, strlen(rcpt_to_two));
@@ -71,16 +89,26 @@ void *send_email(void *threadid) {
     // char *rcpt_to_three = "RCPT TO:<recipient3@example.com>\r\n";
     // write(sockfd, rcpt_to_three, strlen(rcpt_to_three));
 
-    sleep(1);
     // DATA
     char *data = "DATA\r\n";
     write(sockfd, data, strlen(data));
+    fcont = true;
+    while ( fcont ) {
+        read(sockfd, buffer, 1);
+        fcont = buffer[0] == '\n'?false:true;
+    }
 
-    sleep(1);
+
     // Email content
     char *email_body = "From: sender@example.com\r\nTo: recipient@example.com\r\nSubject: Test Email\r\n\r\nThis is a test email.\r\n.\r\n";
     write(sockfd, email_body, strlen(email_body));
+    fcont = true;
+    while ( fcont ) {
+        read(sockfd, buffer, 1);
+        fcont = buffer[0] == '\n'?false:true;
+    }
 
+    sleep(10);
     close(sockfd);
     pthread_exit(NULL);
 }
